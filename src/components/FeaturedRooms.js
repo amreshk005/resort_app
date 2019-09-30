bd7147427674610eaf1dfd0206790fc0d65ea6d5
amreshk005 <amreshk005@gmail.com>
2019-09-30 21:36:38 +0530

    added next feature

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context';
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
     render() {
-        const { name, greeting } = this.context;
+        // const { name, greeting } = this.context;
+        let { loading, featuredRooms: rooms } = this.context;
+        console.log(rooms)
+        rooms = rooms.map(room => {
 
+            return <Room key={room.id} room={room} />
+        })
         return (
-            <div>
-                hello from featured rooms {name}
-            </div>
+            <section className="featured-rooms">
+                <Title title="featured rooms" />
+                <div className="featured-rooms center">
+                    {loading ? <Loading /> : rooms}
+                </div>
+                <Loading />
+
+            </section>
         )
     }
 }
--- src/context.js
+++ src/context.js
@@ -15,20 +15,25 @@ class RoomProvider extends Component {
 
     //getData
     componentDidMount() {
-        let rooms = this.formatData(items)
+        //this.getData
+        let rooms = this.formatData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        })
     }
 
     formatData(items) {
         let tempItems = items.map(item => {
             let id = item.sys.id
-            let images = item.fields.images.map(image => {
-                image.fields.file.url
-            })
+            let images = item.fields.images.map(image => image.fields.file.url);
             let room = { ...item.fields, images, id };
             return room;
         })
-
-        return room;
+        return tempItems;
     }
     render() {
         return (
